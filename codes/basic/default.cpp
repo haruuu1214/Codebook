34cb7559bac3b0135ff14b2bbac5aa6dbcbbe7f3
Yin-Lun Du <louiedu0508@gmail.com>
2026-09-11 23:29:46 +0800

    feat: basic, dsu codes

--- codes/basic/default.cpp
+++ codes/basic/default.cpp
@@ -3,15 +3,17 @@
 #define pii pair<int, int>
 #define F first
 #define S second
+#define pb push_back
 #define all(x) x.begin(), x.end()
-#define siz(x) (int)x.size()
+#define sz(x) (int)x.size()
 #define FOR(i, a, b) for(int i = a; i <= b; ++i)
 #define IO ios::sync_with_stdio(0), cin.tie(0)
 using namespace std;
 void debug() { cerr << endl; }
 template <typename T, typename ...U>
 void debug(T i, U ...j) { cerr << i << ' ', debug(j...); }
-#define test(x...) debug("[" + string(x) + "]", x)
+#define test(...) debug("[" #__VA_ARGS__ "]:", __VA_ARGS__)
+
 signed main() {
 	IO;
 	
